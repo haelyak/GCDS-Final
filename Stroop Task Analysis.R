54e1d12961ff6cd0ced6e3233efbc201155fc066
haelyak <ktsai@hmc.edu>
2022-12-07 20:17:57 -0800

    Added some plots

--- Stroop Task Analysis.R
+++ Stroop Task Analysis.R
@@ -1,5 +1,6 @@
 library(magrittr)
 library(dplyr)
+library(ggplot2)
 
 stroopdat <- readr::read_csv("Stroop_agg.csv")
 
@@ -7,11 +8,13 @@ quantile(stroopdat$rt)
 
 stroopdat <- stroopdat %>% filter(., trialtype == "congruent")
 
-view(stroopdat)
 
 view(stroopdat <- stroopdat %>%
   group_by(id) %>%
   mutate(q25 = quantile(stroopdat$rt, 0.25)) %>%
   filter(rt <= q25))
 
-
+# A really basic boxplot.
+ggplot(stroopdat, aes(x =id, y=rt)) + 
+  geom_boxplot(fill="skyblue", alpha=0.2) +
+  geom_jitter(color="black", size=1, alpha=0.9)
